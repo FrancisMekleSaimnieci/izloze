45fbf902ca53cdf505c08c4109ae73afba185101
FrancisMekleSaimnieci <francis.pulins@gmail.com>
2022-03-08 11:14:31 +0200

    tresais

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const vardi=['Roberts Kamols', 'Reinis Melgailis','Kaspars Ķirsis','Daniels Kazainis','Francis Pūliņš'];
+const vardi=['Rober Kamol', 'Reiny Melgailis','Kaspars Ķirsis','Daniel Kazainis','Francis Pūliņš'];
 const balvas=['Galds','Durvju Zvans','Krēsls ar divām kājām','Kviešu grauds','Bērns'];
 const naudaKopa= 30;
 let uzvaretajuSkaits = 5;
-console.log(vardi[rand]);
-Math.random=Math.random() *10;
-rand=Math.floor(rand);
+let rindas=document.querySelector('.rindas');
+//Math.random=Math.random()*10;
+for (let i=0;i<uzvaretajuSkaits;i++){
+    let rand=Math.random()*vardi.length;
+    rand=Math.floor(rand);
+    let uzvaretajs = vardi[rand];
+    rindas.innerHTML+=``
+}
+
+
+
+ 
